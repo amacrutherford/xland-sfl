9f40c7d4633b4636e0f4a7e1c3e08193e3f72bce
alex <amacrutherford@gmail.com>
2024-08-25 14:15:34 +0100

    rm

--- training/utils.py
+++ training/utils.py
@@ -158,53 +158,6 @@ def rollout(
     final_carry = jax.lax.while_loop(_cond_fn, _body_fn, init_val=init_carry)
     return final_carry[1]
 
-def rollout_eval(
-    rng: jax.Array,
-    env: Environment,
-    env_params: EnvParams,
-    init_timestep,
-    train_state: TrainState,
-    init_hstate: jax.Array,
-    num_consecutive_episodes: int = 1,
-) -> RolloutStats:
-
-    def _cond_fn(carry):
-        rng, stats, timestep, prev_action, prev_reward, hstate = carry
-        return jnp.less(stats.episodes, num_consecutive_episodes)
-
-    def _body_fn(carry):
-        rng, stats, timestep, prev_action, prev_reward, hstate = carry
-
-        rng, _rng = jax.random.split(rng)
-        dist, _, hstate = train_state.apply_fn(
-            train_state.params,
-            {
-                "observation": timestep.observation[None, None, ...],
-                "prev_action": prev_action[None, None, ...],
-                "prev_reward": prev_reward[None, None, ...],
-            },
-            hstate,
-        )
-        action = dist.sample(seed=_rng).squeeze()
-        timestep = env.step(env_params, timestep, action, init_timestep)
-
-        stats = stats.replace(
-            success=stats.success + jnp.where(timestep.discount == 0.0, 1, 0),
-            reward=stats.reward + timestep.reward,
-            length=stats.length + 1,
-            episodes=stats.episodes + timestep.last(),
-        )
-        carry = (rng, stats, timestep, action, timestep.reward, hstate)
-        return carry
-
-    # timestep = env.reset(env_params, rng)
-    prev_action = jnp.asarray(0)
-    prev_reward = jnp.asarray(0)
-    init_carry = (rng, RolloutStats(), init_timestep, prev_action, prev_reward, init_hstate)
-
-    final_carry = jax.lax.while_loop(_cond_fn, _body_fn, init_val=init_carry)
-    return final_carry[1]
-
 # @partial(jax.jit, static_argnames=("num_steps",))
 def rollout_nsteps(
     rng: jax.Array,
